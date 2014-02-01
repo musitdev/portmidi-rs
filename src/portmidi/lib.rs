0309d6a91c14907264896f102bdb3b54f1f82556
musitdev <philippe.delrieu@free.fr>
2014-02-01 16:46:48 +0100

    correct rustpkg buid process

--- src/portmidi/lib.rs
+++ src/portmidi/lib.rs
@@ -1,4 +1,4 @@
-#[crate_id = "portmidi#0.0.1"];
+#[crate_id = "portmidi#0.1"];
 #[comment = "PortMidi binding for Rust"];
 #[license = "MIT"];
 #[crate_type = "lib"];
@@ -6,7 +6,8 @@
 ///  build : rustpkg build portmidi
 ///  test : rustpkg test portmidi
 
-extern mod extra;
+//extern mod extra;
+extern mod extra = "extra#0.10-pre";
 
 pub mod midi;
 pub mod time;
