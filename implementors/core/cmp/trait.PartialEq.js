67325474f81e71d0feeadea83bd2102f586257c1
Sam Doshi <sam@metal-fish.co.uk>
2015-01-05 08:44:46 +0000

    Update documentation

---
+++ implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['portmidi'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='portmidi/enum.PortMidiError.html' title='portmidi::PortMidiError'>PortMidiError</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='portmidi/struct.MidiMessage.html' title='portmidi::MidiMessage'>MidiMessage</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='portmidi/struct.MidiEvent.html' title='portmidi::MidiEvent'>MidiEvent</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/fmt/trait.Show.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['portmidi'] = ["<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='enum' href='portmidi/enum.PortMidiError.html' title='portmidi::PortMidiError'>PortMidiError</a>","<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='portmidi/struct.DeviceInfo.html' title='portmidi::DeviceInfo'>DeviceInfo</a>","<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='portmidi/struct.MidiMessage.html' title='portmidi::MidiMessage'>MidiMessage</a>","<a class='stability Unstable' title='Unstable: I/O and core have yet to be reconciled'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='portmidi/struct.MidiEvent.html' title='portmidi::MidiEvent'>MidiEvent</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
