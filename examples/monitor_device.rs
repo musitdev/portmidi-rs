df294e1f961139ca86de075945f80ccd07794dfa
Andreas Linz <klingt.net@gmail.com>
2016-03-05 21:38:37 +0100

    Fix `monitor_device` example

--- examples/monitor_device.rs
+++ examples/monitor_device.rs
@@ -1,57 +1,14 @@
 extern crate portmidi as pm;
 
-use std::thread::sleep_ms;
-
-use pm::{PortMidiDeviceId, Result};
-use pm::PmError::PmInvalidDeviceId;
-
-pub mod common;
-
 fn main() {
-    // get the device number from the command line, or die()
-    let device_id = match common::get_arg(1) {
-        Some(id) => id,
-        None => {
-            common::die();
-            return;
+    let context = pm::PortMidi::new().unwrap();
+    let info = context.default_input_device_id().and_then(|id| context.device_info(id));
+    println!("{:?}", info);
+    let mut in_port = context.default_input_port().unwrap();
+
+    while let Ok(avail) = in_port.poll() {
+        if let Ok(Some(event)) = in_port.read() {
+            println!("{:?}", event);
         }
-    };
-
-    // run the monitor, print an error if it returns one
-    if let Err(e) = monitor(device_id) {
-        println!("Error: {:?}", e);
     }
-}
-
-fn monitor(device_id: PortMidiDeviceId) -> Result<()> {
-    // initialize portmidi
-    try!(pm::initialize());
-
-    // get the device and check it exists
-    let device = try!(pm::DeviceInfo::new(device_id).ok_or(PmInvalidDeviceId));
-    println!("Opening: {}", device.name);
-
-    // open the input
-    let mut input = pm::InputPort::new(device_id, 1024);
-    try!(input.open());
-
-    let qw = common::QuitWatcher::new();
-    qw.start();
-
-    while qw.is_running() {
-        // read all the data in the buffer
-        while let Some(event) = try!(input.read()) {
-            // filter midi time stamps and active sensing
-            if event.message.status != 248 && event.message.status != 254 {
-                println!("{} {:?}", event.timestamp, event.message);
-            }
-        }
-
-        // no more data? wait a little before trying again
-        sleep_ms(50);
-    }
-
-    // close the input and terminate portmidi
-    try!(input.close());
-    pm::terminate()
 }
