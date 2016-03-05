be1e4553395f8eef4ed0cb48d43191278791b431
Andreas Linz <klingt.net@gmail.com>
2016-03-05 20:44:54 +0100

    Make optional parameters (can be NULL) to `Option`s

--- src/ffi/functions.rs
+++ src/ffi/functions.rs
@@ -22,17 +22,17 @@ extern "C" {
     pub fn Pm_GetDeviceInfo(id: PmDeviceId) -> *const PmDeviceInfo;
     pub fn Pm_OpenInput(stream: *const *const PortMidiStream,
                         inputDevice: PmDeviceId,
-                        inputDriverInfo: *const c_void,
+                        inputDriverInfo: Option<*const c_void>,
                         bufferSize: i32,
-                        time_proc: *const c_void,
-                        time_info: *const c_void)
+                        time_proc: Option<*const c_void>,
+                        time_info: Option<*const c_void>)
                         -> PmError;
     pub fn Pm_OpenOutput(stream: *const *const PortMidiStream,
                          outputDevice: PmDeviceId,
-                         inputDriverInfo: *const c_void,
+                         inputDriverInfo: Option<*const c_void>,
                          bufferSize: i32,
-                         time_proc: *const c_void,
-                         time_info: *const c_void,
+                         time_proc: Option<*const c_void>,
+                         time_info: Option<*const c_void>,
                          latency: i32)
                          -> PmError;
     pub fn Pm_Read(stream: *const PortMidiStream, buffer: *mut PmEvent, length: i32) -> i32;
